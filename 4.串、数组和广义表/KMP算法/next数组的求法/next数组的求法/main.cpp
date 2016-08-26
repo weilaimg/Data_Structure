8c1c8b9dfe4cb31a3440030766c55cd67c159a14
weilaimg1314 <weilaimg1314@gmail.com>
2016-08-26 15:22:37 +0800

    前后缀匹配函数

--- 4.串、数组和广义表/KMP算法/next数组的求法/next数组的求法/main.cpp
+++ 4.串、数组和广义表/KMP算法/next数组的求法/next数组的求法/main.cpp
@@ -10,3 +10,46 @@
 #include <string.h>
 using namespace std;
 
+void get_next (char T[] , int *next);
+
+void get_text (const char T[] , int text[]){
+    int i , j;
+    i = 0;j =1;
+    text[0] = 0;
+    while(j<strlen(T)){
+        
+        if(T[i] == T[j]){
+            
+            i++;
+            text[j] = i;
+            j++;
+            
+        } else {
+            
+            i=0;
+            if(T[i]!=T[j]){
+                text[j] = 0;
+                j++;
+            }
+        
+        }
+    }
+}
+
+void test(){
+    char a[100];
+    cout << "a = ";
+    cin >> a;
+    int test[strlen(a)];
+    get_text(a, test);
+    cout << "b = ";
+    for (int i = 0;i < strlen(a);i++){
+        cout << test[i];
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    while (1) test();
+}
