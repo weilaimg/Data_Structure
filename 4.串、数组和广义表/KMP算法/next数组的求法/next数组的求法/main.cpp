f288714dbee0b0d36ca2d26f1112fd8982469165
weilaimg1314 <weilaimg1314@gmail.com>
2016-08-26 12:44:47 +0800

    Next 数组求法

---
+++ 4.串、数组和广义表/KMP算法/next数组的求法/next数组的求法/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  next数组的求法
+//
+//  Created by WeiLai’s Macbook Pro on 8/26/16.
+//  Copyright © 2016 WeiLai’s Macbook Pro. All rights reserved.
+//
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
